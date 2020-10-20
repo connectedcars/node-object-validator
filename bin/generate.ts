1c871e6e628c1750760fa89d173e3f345e2539b2
Troels Liebe Bentsen <troels@connectedcars.io>
2020-10-20 23:44:54 +0200

    Add more checks for types

--- bin/generate.ts
+++ bin/generate.ts
@@ -21,18 +21,31 @@ async function main(argv: string[]): Promise<number> {
     ts.forEachChild(sourceFile, node => {
       if (ts.isInterfaceDeclaration(node) || ts.isTypeAliasDeclaration(node)) {
         console.log(`interface ${node.name.text} {`)
-        const type = checker.getTypeAtLocation(node.name)
-        for (const prop of type.getProperties()) {
-          const type = checker.typeToString(checker.getTypeOfSymbolAtLocation(prop, prop.valueDeclaration))
-          console.log(`  ${prop.name}: ${type}`)
+        const interfaceType = checker.getTypeAtLocation(node.name)
+        for (const prop of interfaceType.getProperties()) {
+          const propType = checker.getTypeOfSymbolAtLocation(prop, prop.valueDeclaration)
+          if (propType.isClassOrInterface()) {
+            console.log(`  ${prop.name}: subtype`)
+          } else if (propType.isTypeParameter()) {
+            console.log(`  ${prop.name}: generic`)
+          } else {
+            const typeName = checker.typeToString(propType)
+            console.log(`  ${prop.name}: ${typeName}`)
+          }
         }
         console.log('}')
       }
     })
   }
-
   return 0
 }
+
+/*
+function typeToValidator(type: string) {
+  switch (type) {
+    case 'string':
+  }
+}*/
 
 main(process.argv)
   .then(exitCode => {
