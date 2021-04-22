d2fa52d8a4535417a768455ecd48d459cfece7ab
Troels Liebe Bentsen <tlb@connectedcars.io>
2021-04-22 15:24:25 +0200

    Require 4 digit year for datetime

--- src/validators/datetime.ts
+++ src/validators/datetime.ts
@@ -10,7 +10,7 @@ export function isDateTime(value: unknown, context?: string): value is string {
   return false
 }
 
-const dateTimePattern = /^([0-9]+)-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])[Tt]([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9]|60)(\.[0-9]+)?(([Zz])|([+|-]([01][0-9]|2[0-3]):[0-5][0-9]))$/
+const dateTimePattern = /^([0-9]{4})-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])[Tt]([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9]|60)(\.[0-9]+)?(([Zz])|([+|-]([01][0-9]|2[0-3]):[0-5][0-9]))$/
 
 export function validateDateTime(value: unknown, context?: string): ValidationFailure[] {
   const stringError = validateString(value, 20, 30, context)
